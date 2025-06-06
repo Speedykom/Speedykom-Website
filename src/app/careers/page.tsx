9983f9b81fb28869f3c108fc0c01c594b44387da
Med Dhia Kassab <dhia.kassab@speedykom.de>
2025-06-06 01:08:45 +0100

    add Careers and Contact pages

---
+++ src/app/careers/page.tsx
@@ -0,0 +1,11 @@
+import CareersBanner from "@/components/CareersBanner";
+import CompanyCulture from "@/components/CareersSection";
+
+export default function CareersPage() {
+    return (
+        <div>
+            <CareersBanner />
+            <CompanyCulture />
+        </div>
+    );
+}
